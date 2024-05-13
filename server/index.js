a093bf966dc3c7ba0edcfe4f1bf10e703e7433fb
Ashutosh Kumar <97340498+ashutoshgithubs@users.noreply.github.com>
2024-05-14 04:12:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-socket-henna.vercel.app/",
+    origin: "https://chat-socket-henna.vercel.app",
     methods: ["GET", "POST"],
   },
 });
